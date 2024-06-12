457b0d9965fb3bba8a10a4f9599d5ba33748af9c
Jerry Pierre <jpier132@fiu.edu>
2024-06-11 22:10:54 -0400

    API GET proxy works

--- server.js
+++ server.js
@@ -1,41 +1,56 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 const bodyParser = require('body-parser');
-const PORT = 3036;
-const API_BASE_URL = 'https://take-home-assessment-423502.uc.r.appspot.com/api';
+const PORT = 5000;
+const EXT_API_BASE_URL = 'https://take-home-assessment-423502.uc.r.appspot.com/api';
 
 app.use(bodyParser.json());
 app.use(cors());
 
-//Get videos by user_id   
+// Proxy to get videos by user_id   
 app.get('/api/videos', async (req, res) => {
-//add try-catch error handling
+    const {user_id} = req.query;    
+    //add try-catch error handling
+    try{
+        const response = await axios.get(`${EXT_API_BASE_URL}/videos`, {params: {user_id}});
+        //Handle the response here
+        res.json(response.data);
+    } catch (error) {
+        //Handle the error here
+        res.status(422).json({ error: 'Error fetching videos' });
+    }
 });
 
-//Create a new video
+//proxy to create a new video
 app.post('/api/videos', async (req, res) => {
-//add try-catch error handling
+    //add try-catch error handling
+    try{} catch (error) {}
 });
 
-//Upadate video
+//Proxy to update video
 app.put('/api/videos', async (req, res) => {
-//add try-catch error handling
+    //add try-catch error handling
+    try{} catch (error) {}
 });
 
-//Get single video by video_id
+//proxy to get single video by video_id
 app.get('/api/videos/single', (req, res) => {
-//add try-catch error handling
+    //add try-catch error handling
+    try{} catch (error) {}
 });
 
-//Get comments for video
+//proxy to get comments for video
 app.get('/api/videos/comments', (req, res) => {
-//add try-catch error handling
+    //add try-catch error handling
+    try{} catch (error) {}
 });
 
-//create a new comment for video
+//proxy to create a new comment for video
 app.post('/api/videos/comments', (req, res) => {
-//add try-catch error handling
+    //add try-catch error handling
+    try{} catch (error) {}
 });
 
 
